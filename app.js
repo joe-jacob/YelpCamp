9ed5dea1ed120869b523b1755ce41ebc8aab8f5d
Joe Jacob <joejacob2402@gmail.com>
2026-02-13 17:33:26 +0530

    campground changes

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express  = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const {CampgroundSchema} = require('./schemas.js');
 const ejsMate = require('ejs-mate');
 const wrapAsync = require('./Utils/wrapAsync');
 const ExpressError = require('./Utils/ExpressError')
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const { wrap } = require('module');
+const { title } = require('process');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(()=>{
         console.log("Database connected successfully");
+        
     })
     .catch(error => handleError(error));
 
@@ -28,7 +32,18 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true })); //for access to req.body
 app.use(methodOverride('_method')); //to use post delete etc methods in form
-            
+
+const validateCampground = (req,res,next) =>{
+    const {error} = CampgroundSchema.validate(req.body);
+    const errorMessage = error.details.map(el=> el.message).join(',')
+    if(error) {
+        throw new ExpressError(errorMessage, 400);
+    }
+    else{
+        next();
+    }
+}
+
 app.get('/', (req,res) =>{
      res.render('campgrounds/home')
 });
@@ -42,10 +57,10 @@ app.get('/campgrounds/new', (req,res) =>{
     res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', wrapAsync(async(req,res,next) =>{
-    if(!req.body.campground) throw new ExpressError('Invalid data', 400); //we have client side validation but this will make sure even if it's done through postman it won't break
+app.post('/campgrounds', validateCampground, wrapAsync(async(req,res,next) =>{
+    //if(!req.body.campground) throw new ExpressError('Invalid data', 400); //we have client side validation but this will make sure even if it's done through postman it won't break
     const campground = new Campground(req.body.campground)
-    await campground.save()
+    await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 }));
 
@@ -59,16 +74,25 @@ app.get('/campgrounds/:id/edit', wrapAsync(async(req,res,next) =>{
     res.render('campgrounds/edit', { campground })
 })); 
 
-app.put('/campgrounds/:id', wrapAsync(async(req,res,next) =>{
+app.put('/campgrounds/:id', validateCampground, wrapAsync(async(req,res,next) =>{
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-app.delete('/campgrounds/:id', wrapAsync(async(req,res,next) => {
+app.delete('/campgrounds/:id', wrapAsync(async(req,res) => {
     await Campground.findByIdAndDelete(req.params.id);
     res.redirect('/campgrounds');
 })); 
+
+app.post('/campgrounds/:id/reviews', validateCampground, wrapAsync(async(req,res,next) =>{
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
 
 app.all(/(.*)/, (req,res,next) => {
     next(new ExpressError('Page Not Found', 404));
